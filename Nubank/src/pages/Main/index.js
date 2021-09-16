c4a04146a08f10e8e7fc1940bda74c3bec9d45ac
Gustavo Baringer <gustavobaringer@gmail.com>
2021-09-15 22:31:31 -0300

    card animation done

--- Nubank/src/pages/Main/index.js
+++ Nubank/src/pages/Main/index.js
@@ -1,32 +1,78 @@
 import React from 'react';
-import Icon from 'react-native-vector-icons/MaterialIcons'
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import Header from '~/components/Header/'
-import Tabs from '~/components/Tabs/'
+import { Animated } from 'react-native'
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 
-import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles'
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
+
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+} from './styles';
 
 export default function Main() {
+  const translateY = new Animated.Value(0)
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        }
+      }
+    ],
+    { useNativeDriver: true }
+  )
+
+  function onHandlerStateChange(event) {
+
+  }
+
   return (
     <Container>
       <Header />
 
       <Content>
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description>R$ 98.564,89</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 1794,00 recebida de Gustavo Baringer hoje às 08:03h
-            </Annotation>
-          </CardFooter>
-        </Card>
+        <Menu translateY={translateY} />
+
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChange}
+        >
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [-200, 0, 380],
+                outputRange: [-50, 0, 380],
+                extrapolate: 'clamp'
+              })
+            }]
+          }}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 98.564,89</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 1794,00 recebida de Gustavo Baringer hoje às 08:03h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
 
       <Tabs />
